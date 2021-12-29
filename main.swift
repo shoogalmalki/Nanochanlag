8101b456258502c6613e4677fdb41972477f7242
shoogalmalki <96816650+shoogalmalki@users.noreply.github.com>
2021-12-30 00:33:56 +0300

    Add files via upload

---
+++ main.swift
@@ -0,0 +1,105 @@
+//
+//  main.swift
+//  Hello CLI
+//
+//  Created by shoog almalki on 19/05/1443 AH.
+//
+
+import Foundation
+
+
+print("------------------------------------------")
+print("with pride flay the most improved Airline in the world for 2021")
+print("-------------MENU----------")
+print("1. BOOK A TRIP")
+print("2. MANAGE BOOKINGS")
+print("3. CHECK IN ONLINE")
+print("FLIGHT INFO")
+print("------------------------------------------")
+
+
+if var userChoice = readLine(){
+    let days = ["Sunday","Monday","Tuseday","Wednsday","Thursday","Friday","Saturday"]
+    let list = days.joined(separator: ", ")
+    print(list)
+    
+default:
+    print("")
+}
+      print("please choose Round trip")
+
+     _ = ["Riyadh,Tabuk","Tabuk","Dammam"]
+
+print("pleas Enter The date")
+  
+var date = readLine()!
+print("pleas Enter the number of passengers")
+
+var numberofpassengers = readLine()!
+print("pleas Select your Flight Class (Economy,Business,First Class")
+
+var flightClass = readLine()!
+
+  
+
+
+print("Avaiable Flight")
+ 
+var    AvailableFlights = readLine()
+
+print("Please choose appropate Flight")
+
+print("please Enter your personal in formation 1- Full Name 2- date of birth 3- Ad hte number")
+
+var info = readLine()
+ 
+var Class = readLine()
+
+
+let days = ["Sunday","Monday","Tuseday","Wednsday","Thursday","Friday","Saturday"]
+
+
+
+var Totalprice = readLine()!
+print("total price is 650")
+
+var flightprice = 990
+
+if flightprice > 1000
+{
+      print("Free Order")
+}
+else if flightprice < 900 && flightprice <= 1100 {
+print("NO Free Order")
+    
+    print("Seat selection")
+    
+    print("------In the event that the passenger's temperature is more htan 38 degrees,fly apologizes for transporting the passenger and the entire value of the ticket will be saved as a credit valid for use for one year-------")
+
+    
+    
+    print("How many bags do you have")
+    var bags = Utils.readInt()
+    
+    
+    
+    
+}
+
+func flightInformation() {
+    print("Your Booking information✈️")
+}
+
+func Availableflights() {
+    print("fly Id:XY 149 Riyadh 12:40;aljoof 15:15")
+    
+}
+func SelectClass() {
+    
+    var capitalCity = ["Busiess":"1900","first Class":"2500","Economic":"460"]
+    print(capitalCity)
+}
+
+print("Exit")
+print("---✈️✈️---\n----🛫----\nYour reservation info is \(reservation)\n-----🛬--------\n--------")
+
